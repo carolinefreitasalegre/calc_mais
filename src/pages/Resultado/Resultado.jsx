0fd279818a2f8c8cff3053ef5bf6b46469db5ad3
Caroline <113471098+carolinefreitasalegre@users.noreply.github.com>
2023-08-18 21:14:45 -0400

    Update Resultado.jsx

--- src/pages/Resultado/Resultado.jsx
+++ src/pages/Resultado/Resultado.jsx
@@ -3,6 +3,8 @@ import ButtonGo from "../../components/button/ButtonGo";
 import { ImcContext, ImcProvider } from "../../contexts/ImcContext";
 import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import IMG from "../../../public/assets/tabela.png";
+
 
 export default function Final() {
   const { nome, altura, peso, imc } = useContext(ImcContext);
@@ -22,7 +24,7 @@ export default function Final() {
               <p>Peso: {peso}</p>
               <p>Altura: {altura}</p>
             </Result>
-            <Image src="../../../public/assets/tabela.png" />
+            <Image src={IMG}/>
           </Text>
           <div style={{ margin: "15em 0 0 1em", zIndex: 1000 }}>
             <Link
